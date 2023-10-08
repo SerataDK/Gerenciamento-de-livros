3c1a81f092ab39590ca74fd03bbe6a0631b98758
SerataDK <120990739+SerataDK@users.noreply.github.com>
2023-10-08 18:00:03 -0300

    Entrega01(gui): GUI de login sem funcionalidades

---
+++ projetoa3/src/main/java/com/mycompany/projetoa3/Projetoa3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa3;
+
+/**
+ *
+ * @author Dukat
+ */
+public class Projetoa3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
